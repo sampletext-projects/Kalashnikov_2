1d224ba91c4ca2f1cce06b84cbb2c5d26b7ee3d2
sampletext32 <sampletext32@bk.ru>
2020-06-25 22:31:01 +0300

    Add project

---
+++ Kalashnikov_2/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	double a, b, c, e;
+	double fa, fc;
+
+	cin >> a;
+	cin >> b;
+	cin >> e;
+
+	int i = 0;
+	fa = sqrt(1 - 0.4 * a * a) - asin(a);
+	c = (a + b) / 2;
+	fa = sqrt(1 - 0.4 * c * c) - asin(c);
+
+	while (fabs(a - b) >= e)
+	{
+		if (fa * fc > 0)
+		{
+			a = c;
+			fa = sqrt(1 - 0.4 * a * a) - asin(a);
+		}
+		else
+		{
+			b = c;
+		}
+		c = (a + b) / 2;
+		fa = sqrt(1 - 0.4 * a * a) - asin(a);
+		i = i + 1;
+	}
+	cout << "x= " << c << endl;
+	system("pause");
+}
