3de32cfb242be13626feb7cd579a34659c2cec55
sampletext32 <sampletext32@bk.ru>
2020-06-25 22:36:33 +0300

    Fix calc

--- Kalashnikov_2/main.cpp
+++ Kalashnikov_2/main.cpp
@@ -14,21 +14,21 @@ void main()
 	int i = 0;
 	fa = sqrt(1 - 0.4 * a * a) - asin(a);
 	c = (a + b) / 2;
-	fa = sqrt(1 - 0.4 * c * c) - asin(c);
+	fc = sqrt(1 - 0.4 * c * c) - asin(c);
 
 	while (fabs(a - b) >= e)
 	{
 		if (fa * fc > 0)
 		{
 			a = c;
-			fa = sqrt(1 - 0.4 * a * a) - asin(a);
+			fa = a + sqrt(a) + pow(a, 1 / 3.) - 2.5;
 		}
 		else
 		{
 			b = c;
 		}
 		c = (a + b) / 2;
-		fa = sqrt(1 - 0.4 * a * a) - asin(a);
+		fc = sqrt(1 - 0.4 * c * c) - asin(c);
 		i = i + 1;
 	}
 	cout << "x= " << c << endl;
